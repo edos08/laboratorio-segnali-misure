653d8d2f3d9d7b6a7f581587ee994e88021ef968
leobeltra <99175159+leobeltra@users.noreply.github.com>
2022-02-07 10:50:37 +0100

    Update BME_280

--- main/BME_280.ino
+++ main/BME_280.ino
@@ -1,7 +1,54 @@
+#include <Wire.h>
+
+#include <Adafruit_Sensor.h>
+
+#include <Adafruit_BME280.h>
+
+#define SEALEVELPRESSURE_HPA (1013.25)
+
+Adafruit_BME280 bme;
+
+unsigned long delayTime;
+
 void setupBME280() {
+  Serial.println(F("BME280 test"));
+
+  bool status = bme.begin();  
+  if (!status) {
+    Serial.println("Could not find a valid BME280 sensor, check wiring!");
+    while (1);
+  }
   
+  Serial.println("-- Default Test --");
+ 
+  delayTime = 1000;
+
+  Serial.println();
 }
 
 void get_BME280_data() {
+   Serial.print("Temperature = ");
+  Serial.print(bme.readTemperature());
+  Serial.println(" *C");
+
+  // Convert temperature to Fahrenheit
+  /*Serial.print("Temperature = ");
+  Serial.print(1.8 * bme.readTemperature() + 32);
+  Serial.println(" *F");*/
   
+  Serial.print("Pressure = ");
+  Serial.print(bme.readPressure() / 100.0F);
+  Serial.println(" hPa");
+  
+  Serial.print("Approx. Altitude = ");
+  Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
+  Serial.println(" m");
+  
+  Serial.print("Humidity = ");
+  Serial.print(bme.readHumidity());
+  Serial.println(" %");
+  
+  delay(delayTime);
+  
+  Serial.println();
 }
